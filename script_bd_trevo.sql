e6e8255733522933f8f4346ca50caf94f00e197a
allantak <allanxd132@gmail.com>
2023-04-04 11:50:52 -0300

    script bd_trevo

--- script_bd_trevo.sql
+++ script_bd_trevo.sql
@@ -1,48 +1,45 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-DROP TABLE MANAGER CASCADE;
-DROP TABLE CLIENT CASCADE;
+DROP TABLE ACCOUNT CASCADE;
 DROP TABLE PRODUCT CASCADE;
 DROP TABLE ORDER_ITEM;
 DROP TABLE CULTURE;
 DROP TABLE IMAGE;
 
-CREATE TABLE CLIENT (
-	client_id uuid DEFAULT uuid_generate_v4 (),
-	client_name varchar(50) NOT NULL,
+CREATE TABLE ACCOUNT (
+	account_id uuid DEFAULT uuid_generate_v4 (),
+	account_name varchar(50) NOT NULL,
 	email varchar(50) NOT NULL UNIQUE,
-	phone varchar(30) NOT NULL UNIQUE,
-	PRIMARY KEY (client_id)
+	account_password varchar NOT NULL,
+	create_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	account_role varchar(50) NOT NULL,
+	PRIMARY KEY (account_id)
 );
 
-CREATE TABLE MANAGER (
-	manager_id uuid DEFAULT uuid_generate_v4 (),
-	username varchar(50) NOT NULL UNIQUE,
-	manager_password varchar NOT NULL,
-	PRIMARY KEY (manager_id)
-);
 
 CREATE TABLE PRODUCT (
 	product_id uuid DEFAULT uuid_generate_v4 (),
-	manager_id uuid NOT NULL,
+	account_id uuid NOT NULL,
 	product_name varchar(50) NOT NULL UNIQUE,
 	area_size real,
+	price real,
 	description text NOT NULL,
-	create_at date DEFAULT CURRENT_DATE NOT NULL,
-	status boolean NOT NULL,
+	create_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	status varchar(50) NOT NULL,
+	category varchar(50) NOT NULL,
 	PRIMARY KEY (product_id),
-	FOREIGN KEY (manager_id) REFERENCES MANAGER (manager_id)
+	FOREIGN KEY (account_id) REFERENCES ACCOUNT (account_id)
 );
 
 
 CREATE TABLE ORDER_ITEM (
 	order_item_id uuid DEFAULT uuid_generate_v4 (),
 	product_id uuid NOT NULL,
-	client_id uuid NOT NULL,
+	account_id uuid NOT NULL,
 	quantity int NOT NULL,
 	PRIMARY KEY (order_item_id),
 	FOREIGN KEY (product_id) REFERENCES PRODUCT (product_id),
-	FOREIGN KEY (client_id) REFERENCES CLIENT (client_id)
+	FOREIGN KEY (account_id) REFERENCES ACCOUNT (account_id)
 );
 
 CREATE TABLE CULTURE (
@@ -56,13 +53,12 @@ CREATE TABLE CULTURE (
 CREATE TABLE IMAGE (
 	image_id uuid DEFAULT uuid_generate_v4 (),
 	product_id uuid NOT NULL,
-	img bytea NOT NULL,
+	img varchar NOT NULL,
 	PRIMARY KEY (image_id),
 	FOREIGN KEY (product_id) REFERENCES PRODUCT (product_id)
 );
 
-INSERT INTO MANAGER (username, manager_password) VALUES ('admin','$2a$12$//unR2JTZisNleMtuYYFS.Ewh.vXLqK.gwpC9JyAfCKpLqhLOvlnW');
-
+INSERT INTO ACCOUNT (email, account_name, account_password, account_role) VALUES ('admin@gmail.com', 'admin','$2a$12$//unR2JTZisNleMtuYYFS.Ewh.vXLqK.gwpC9JyAfCKpLqhLOvlnW', 'ADMINISTRADOR');
 	
 
 
